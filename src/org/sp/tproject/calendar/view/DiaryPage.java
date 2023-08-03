0160c68d97e9893a8d5900e52d39dad64a3aca61
darae <darae729@gmail.com>
2023-08-03 11:46:37 +0900

    다이어리 추가

--- src/org/sp/tproject/calendar/view/DiaryPage.java
+++ src/org/sp/tproject/calendar/view/DiaryPage.java
@@ -1,7 +1,34 @@
 package org.sp.tproject.calendar.view;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import org.sp.tproject.main.view.Page;
 
 public class DiaryPage extends Page{
-
+	JPanel p_north;
+	JPanel p_west;
+	JPanel p_center;
+	JPanel p_east;
+	JButton bt_prev;
+	JButton bt_next;
+	JLabel la_title;
+	
+	public DiaryPage() {
+		//UI 생성하기
+		p_north = new JPanel();
+		p_west = new JPanel();
+		p_center = new JPanel();
+		p_east = new JPanel();
+		bt_prev = new JButton();
+		bt_next = new JButton();
+		la_title = new JLabel();
+		
+		
+	}
 }
+
+
+
+
